74538901ccca3b155886f6ed320876624141fdf4
driveways <keithpsmith@gmail.com>
2017-07-18 01:51:05 -0700

    Add feed parser.

---
+++ src/main/kotlin/Filing.kt
@@ -0,0 +1,22 @@
+import kotlin.reflect.full.memberProperties
+
+class Filing {
+    var accessionNumber: String? = null
+    var formType: String? = null
+    var filingDate: Number? = null
+    var acceptanceDate: String? = null
+    var cikNumber: Number? = null
+    var enclosureUrl: String? = null
+    var sic: Number? = null
+    var companyName: String? = null
+
+
+    override fun toString(): String {
+        var s = "{"
+        for (prop in Filing::class.memberProperties) {
+            s += "\"${prop.name}\"=\"${prop.get(this)}\","
+        }
+        s.removeSuffix(",")
+        return s + "}"
+    }
+}
---
+++ src/main/kotlin/StreamProcessor.kt
@@ -0,0 +1,60 @@
+import java.text.SimpleDateFormat
+import javax.xml.stream.XMLStreamReader
+
+class StreamProcessor constructor(val reader: XMLStreamReader) {
+
+    private val tagItem = "item"
+    private val tagAccessionNumber = "edgar:accessionNumber"
+    private val tagFormType = "edgar:formType"
+    private val tagFilingDate = "pubDate"
+    private val tagAcceptanceDate = "edgar:acceptanceDatetime"
+    private val tagCikNumber = "edgar:cikNumber"
+    private val tagEnclosure = "enclosure"
+    private val tagSic = "edgar:assignedSic"
+    private val tagCompanyName = "edgar:companyName"
+
+    private val feedTimeFormat = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz")
+
+    private val filings: MutableList<Filing> = mutableListOf()
+    private var curFiling: Filing = Filing()
+
+    fun processAll() {
+        while (reader.hasNext()) {
+            when (reader.next()) {
+                XMLStreamReader.START_ELEMENT -> startElement()
+                XMLStreamReader.END_ELEMENT -> endElement()
+            }
+        }
+    }
+
+    private fun prettyName(): String {
+        val prefix = reader.prefix
+        if (prefix != "") {
+            return "$prefix:${reader.localName}"
+        } else {
+            return reader.localName
+        }
+    }
+
+    private fun startElement() {
+        when (prettyName()) {
+            tagItem -> {curFiling = Filing()}
+            tagAccessionNumber -> {curFiling.accessionNumber = reader.elementText}
+            tagFormType -> {curFiling.formType = reader.elementText}
+            tagFilingDate -> {curFiling.filingDate = feedTimeFormat.parse(reader.elementText).time}
+            tagAcceptanceDate -> {curFiling.acceptanceDate = reader.elementText}
+            tagCikNumber -> {curFiling.cikNumber = Integer.parseInt(reader.elementText)}
+            tagEnclosure -> {curFiling.enclosureUrl = reader.getAttributeValue(null, "url")}
+            tagSic -> {curFiling.sic = Integer.parseInt(reader.elementText)}
+            tagCompanyName -> {curFiling.companyName = reader.elementText}
+        }
+    }
+    private fun endElement() {
+        when (prettyName()) {
+            tagItem -> {filings.add(curFiling)
+                println(curFiling.toString())
+            }
+        }
+    }
+
+}
